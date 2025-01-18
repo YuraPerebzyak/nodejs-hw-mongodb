86d78b9f18448bf1a3dc5150625d75ad7fb420f1
YuraPerebzyak <yura.perebzyak.231998@gmail.com>
2025-01-18 14:26:17 +0200

    ad hw3 node

--- src/server.js
+++ src/server.js
@@ -1,66 +1,31 @@
 import express from 'express';
 import cors from 'cors';
-import pino from 'pino-http';
 
-import * as contactServices from './services/contacts.js';
+import contactsRouter from './routers/contactsRouter.js';
+
 import { getEnvVar } from './utils/getEnvVar.js';
 
-const PORT = Number(getEnvVar('PORT', 3000));
+import { logger } from './middlewares/logger.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 
 export const setupServer = () => {
   const app = express();
 
   app.use(cors());
   app.use(express.json());
-  app.use(
-    pino({
-      transport: {
-        target: 'pino-pretty',
-      },
-    }),
-  );
+  app.use(logger);
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await contactServices.getContact();
+  app.use('/', contactsRouter);
+  app.use('/contacts', contactsRouter);
 
-    res.json({
-      status: 200,
-      message: 'Successfully found contact',
-      data: contacts,
-    });
+  app.use(notFoundHandler);
+
+  app.use(errorHandler);
+
+  const PORT = Number(getEnvVar('PORT', 3000));
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
   });
-
-  app.get('/contacts/:contactId', async (req, res) => {
-    const id = req.params.contactId;
-
-    const contact = await contactServices.getContactByID(id);
-
-    if (!contact) {
-      return res.status(404).json({
-        status: 404,
-        message: `Contact with ${id} not found`,
-      });
-    }
-
-    res.json({
-      status: 200,
-      message: `Successfully found contact with id=${id}`,
-      data: contact,
-    });
-  });
-
-  app.use((req, res) => {
-    res.status(404).json({
-      message: `${req.url} Not found`,
-    });
-  });
-
-  app.use((error, req, res, next) => {
-    res.status(500).json({
-      message: 'Server error',
-      error: error.message,
-    });
-  });
-
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 };
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,11 +1,37 @@
-import contactsCollection from '../db/models/contact.js';
+import ContactCollection from '../db/models/Contact.js';
 
-export const getContact = async () => {
-  const contacts = await contactsCollection.find();
-  return contacts;
+export const getContacts = () => ContactCollection.find();
+
+export const getContactById = (id) => ContactCollection.findById(id);
+
+export const addContact = (contactData) =>
+  ContactCollection.create(contactData);
+
+export const updateContact = async (_id, contactData, options = {}) => {
+  const { upsert = false } = options;
+  const contact = await ContactCollection.findOneAndUpdate(
+    {
+      _id,
+    },
+    contactData,
+    {
+      new: true,
+      upsert,
+      includeResultMetadata: true,
+    },
+  );
+
+  const isNew = Boolean(contact.lastErrorObject.upserted);
+
+  if (!contact || !contact.value) return null;
+
+  return {
+    isNew,
+    data: contact.value,
+  };
 };
 
-export const getContactByID = async (id) => {
-  const contact = await contactsCollection.findById(id);
-  return contact;
-};
+export const deleteContact = async (id) =>
+  ContactCollection.findOneAndDelete(id);
+
+// export const deleteContact = (id) => ContactCollection.findByIdAndDelete(id);
