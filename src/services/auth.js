fb8535f012136e9f7f7cc228c0a29d49712460fa
YuraPerebzyak <yura.perebzyak.231998@gmail.com>
2025-01-27 00:07:42 +0200

    fix import

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,8 +2,8 @@ import createHttpError from 'http-errors';
 import bcrypt from 'bcrypt';
 import { randomBytes } from 'crypto';
 
-import UserCollection from '../db/models/User.js';
-import SessionCollection from '../db/models/Session.js';
+import UserCollection from '../db/models/user.js';
+import SessionCollection from '../db/models/session.js';
 import {
   accessTokenLifetime,
   refreshTokenLifetime,
